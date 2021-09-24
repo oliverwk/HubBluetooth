6b18ecc630a52a845c11d185f6fcd0946164cfd8
øllen <30601311+oliverwk@users.noreply.github.com>
2021-09-24 07:12:15 +0200

    Initial Commit

---
+++ Shared/HubOMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HubOMeterApp.swift
+//  Shared
+//
+//  Created by Maarten Wittop Koning on 24/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct HubOMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
